7910661b6abe0481efe929def797f2a624abf940
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-02-27 01:22:14 +0100

    Home page changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { light, dark } from './palette'
 import ApplicationBar from './modules/ApplicationBar';
 import HomePage from './pages/HomePage';
@@ -10,15 +10,19 @@ import BottomNav from './modules/BottomNav';
 
 let appStyles = {
   container: {
-    width: {xs: '96%', sm: '90%', md: '80%'}
+    maxWidth: { lg: '100%' },
+    width: {xs: '98%', sm: '90%', md: '80%'}
   }
 }
 
 function App() {
   const theme = useSelector(state => state.theme.theme)
+  const muiTheme = responsiveFontSizes(
+    createTheme((theme === 'dark')? dark : light)
+  )
 
   return (
-    <ThemeProvider theme={createTheme((theme === 'dark')? dark : light)}>
+    <ThemeProvider theme={muiTheme}>
       <CssBaseline />
       <ApplicationBar />
       <Container sx={appStyles.container}>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,38 +1,60 @@
 import React from 'react'
 import { Paper, Typography, Box } from '@mui/material';
+import ScooterImage from '../../assets/scooter.png'
+import Image from '../../UI/Image';
+
+const homeStyles = {
+  headerPaper: {
+    paper: {
+      display: 'flex',
+      flexDirection: { xs: 'column', md: 'row' },
+      alignItems: 'center'
+    },
+    box: {
+      width: { xs: '90%', md: '50%' },
+      display: 'flex',
+      alignItems: 'center',
+      p: {xs: 2, lg: 4},
+      pt: {xs: 4, md: 8},
+      pb: {xs: 4, md: 8},
+      flexDirection: 'column',
+      justifyContent: 'center'
+    },
+    imageBox: {
+      width: { xs: '90%', md: '50%' },
+      display: 'flex',
+      alignItems: 'center',
+      p: {xs:4, sm: 8, md: 2, lg: 8},
+    },
+    title: {
+      fontWeight: 'bold',
+      ml: { xs: 2, md: 3, lg: 6},
+      mr: { xs: 2, md: 3, lg: 5}
+    },
+    subtitle: {
+      ml: { xs: 2, md: 3, lg: 6},
+      mr: { xs: 2, md: 3, lg: 5}
+    },
+    image: { width: '100%' }
+  }
+}
 
 export default function HomePage() {
   return (
     <React.Fragment>
-      <Box sx={{ bgcolor: '#cf33fc', height: '40vh' }}>
-        <Typography variant="h1" gutterBottom>
-          Food Delivery
-        </Typography>
-      </Box>
-      <Box>
-        <Paper sx={{ height: '40vh' }}>
-          <Typography variant="h3" gutterBottom>
-            Hello
+      <Paper elevation={3} sx={homeStyles.headerPaper.paper}>
+        <Box sx={homeStyles.headerPaper.box}>
+          <Typography variant="h2" gutterBottom sx={homeStyles.headerPaper.title}>
+            We Provide Delivery Within 30 Min
           </Typography>
-        </Paper>
-      </Box>
-      <Box sx={{ bgcolor: '#cf399c', height: '40vh' }}>
-        <Typography variant="h1" gutterBottom>
-          Food Delivery
-        </Typography>
-      </Box>
-      <Box sx={{ bgcolor: '#cf99fc', height: '40vh' }}>
-        <Typography variant="h1" gutterBottom>
-          Food Delivery
-        </Typography>
-      </Box>
-      <Box>
-        <Paper sx={{ height: '12vh' }}>
-          <Typography variant="h3" gutterBottom>
-            Hello
+          <Typography variant="h5" sx={homeStyles.headerPaper.subtitle}>
+            Imagine you don't need a diet because we provide healthy and delicious food for you!
           </Typography>
-        </Paper>
-      </Box>
+        </Box>
+        <Box sx={homeStyles.headerPaper.imageBox}>
+          <Image src={ScooterImage} sx={homeStyles.headerPaper.image}/>
+        </Box>
+      </Paper>
     </React.Fragment>
   )
 }
