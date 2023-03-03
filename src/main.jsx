3b02a595d886887285f027b775d238a875bc304a
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-03-03 22:13:37 +0100

    HashRouter added

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
 import store from './shared/store';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import './fonts/fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
--- src/router/routesConstants.js
+++ src/router/routesConstants.js
@@ -1,9 +1,7 @@
 
-const GIT_PROJECT = '/react-food-delivery'
+const HOME = '/home'
+const ABOUT_US = '/about-us'
+const NOT_FOUND = '/not-found'
+const CONTACTS = '/contacts'
 
-const HOME = GIT_PROJECT + '/home'
-const ABOUT_US = GIT_PROJECT + '/about-us'
-const NOT_FOUND = GIT_PROJECT + '/not-found'
-const CONTACTS = GIT_PROJECT + '/contacts'
-
-export {HOME, ABOUT_US, NOT_FOUND, CONTACTS, GIT_PROJECT}
+export {HOME, ABOUT_US, NOT_FOUND, CONTACTS}
