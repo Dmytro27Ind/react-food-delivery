ba06dcff5e095ad67e769b8ac6c429ad1810a564
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-03-03 22:34:36 +0100

    hash router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,14 +3,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from '@/pages/HomePage';
 import AboutUs from '@/pages/AboutUsPage'
 import NotFound from '@/pages/NotFound'
-import { ABOUT_US, HOME, NOT_FOUND, GIT_PROJECT } from './routesConstants';
+import { ABOUT_US, HOME, NOT_FOUND } from './routesConstants';
 
 function AppRouter() {
   return (
       <Routes>
-        <Route exact path={HOME} element={<Navigate to={GIT_PROJECT + "/"}/>}/>
+        <Route exact path={HOME} element={<Navigate to="/"/>}/>
         <Route exact path={ABOUT_US} element={<AboutUs/>}/>
-        <Route exact path={GIT_PROJECT + "/"} element={<HomePage/>}/>
+        <Route exact path="/" element={<HomePage/>}/>
 
         <Route exact path={NOT_FOUND} element={<NotFound/>}/>
         {/* 👇️ only match this when no other routes match */}
