7af18388cf11d44bd4b4cf56cd1d1abc84611cef
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-03-03 22:57:29 +0100

    new routes

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './fonts/fonts.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename='/react-food-delivery'>
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
--- src/router/routesConstants.js
+++ src/router/routesConstants.js
@@ -1,7 +1,9 @@
 
-const HOME = '/home'
-const ABOUT_US = '/about-us'
-const NOT_FOUND = '/not-found'
-const CONTACTS = '/contacts'
+const GITHUB_ROUT = '/react-food-delivery'
+
+const HOME = GITHUB_ROUT + '/#' + '/home'
+const ABOUT_US = GITHUB_ROUT + '/#' + '/about-us'
+const NOT_FOUND = GITHUB_ROUT + '/#' + '/not-found'
+const CONTACTS = GITHUB_ROUT + '/#' + '/contacts'
 
 export {HOME, ABOUT_US, NOT_FOUND, CONTACTS}
